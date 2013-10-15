fd900465f3df5d3395b1ec9a413e1bf31e0dd224
Will Kahn-Greene <willkg@mozilla.com>
2013-10-15 16:09:32 -0400

    Add shims so ye' can easily use wit' django
    This adds "translate" and "lint" commands fer manage.py to make it
    trivially easy to use dennis in a django project.

---
+++ dennis/django_dennis/management/commands/translate.py
@@ -0,0 +1,5 @@
+from dennis.django_dennis.management.commands import DennisBaseCommand
+
+class Command(DennisBaseCommand):
+    help = 'Runs dennis subcommands'
+    dennis_subcommand = 'translate'
